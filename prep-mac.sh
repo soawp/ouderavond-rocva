3e65c4d42ad01beb0e8d08ee21d10d4605544452
Tyrone Faulhaber <tyrone@spectrapulse.io>
2019-10-11 22:48:52 +0200

    Updated prep-mac.sh to not display not found error on rm

--- prep-mac.sh
+++ prep-mac.sh
@@ -2,9 +2,9 @@
 # Maak deze script uitvoerbaar door het gebruik van "chmod +x prep-mac.sh"
 # En door vervolgens "./prep-mac.sh" te gebruiken.
 
-rm ./prepare.log                # clean up before we start.
-git fetch --all                 # and make sure the local repo
-git reset --hard origin/master  # is ready and up to date.
+rm ./prepare.log 2> /dev/null	# clean up before we start.
+git fetch --all					# and make sure the local repo
+git reset --hard origin/master	# is ready and up to date.
 git clean -n -f -d
 git pull --force
 
