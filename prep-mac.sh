0fd8b40e8b8f7ace75270a2124c9f8edc1fc0c9a
Tyrone Faulhaber <tyrone@spectrapulse.io>
2019-10-11 22:54:18 +0200

    Updated prep-mac.sh to not display not found error on rm

--- prep-mac.sh
+++ prep-mac.sh
@@ -1,18 +1,20 @@
 #! /bin/bash 
 # Maak deze script uitvoerbaar door het gebruik van "chmod +x prep-mac.sh"
 # En door vervolgens "./prep-mac.sh" te gebruiken.
-
 clear
+
 echo "Voorbereiden..."
 echo "================================================"
+
 rm ./prepare.log 2> /dev/null	# clean up before we start.
 git fetch --all					# and make sure the local repo
 git reset --hard origin/master	# is ready and up to date.
 git clean -n -f -d
-git pull --force
+
 echo ""
 echo "Installeren van extensies..."
 echo "================================================"
+
 install_ext () {
 	echo "Installeren van extensie '$1'..."
 	code -r --install-extension "$1" --force >> ./prepare.log
@@ -21,5 +23,6 @@ install_ext () {
 while read line; do
 	install_ext "${line}"
 done < ./.vscode/extensions.txt
+
 echo ""
 echo "Installatie van extensies voltooid."
