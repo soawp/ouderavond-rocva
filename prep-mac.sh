11d8cf13a76515aaae5da7f0575e53213d8d3b19
Tyrone Faulhaber <tyrone@spectrapulse.io>
2019-10-17 08:05:31 +0200

    Latest Changes

--- prep-mac.sh
+++ prep-mac.sh
@@ -10,6 +10,7 @@ rm ./prepare.log 2>/dev/null   # clean up before we start.
 git fetch --all                # and make sure the local repo
 git reset --hard origin/master # is ready and up to date.
 git clean -f -d
+mkdir ./.vscode/ext
 
 echo ""
 echo "Installeren van extensies..."
@@ -17,7 +18,7 @@ echo "================================================"
 
 install_ext() {
 	echo "Installeren van extensie '$1'..."
-	code -r --install-extension "$1" --force >>./prepare.log
+	code -r --install-extension "$1" --force --extensions-dir "./.vscode/ext" >>./prepare.log
 }
 
 while read line; do
@@ -26,3 +27,4 @@ done <./.vscode/extensions.txt
 
 echo ""
 echo "Alles is voltooid."
+code . --user-data-dir "./.vscode/" --extensions-dir "./.vscode/ext" "./opdracht/index.html" "./opdracht/style.css"
--- win-req/install_ext.ps1
+++ win-req/install_ext.ps1
@@ -7,14 +7,17 @@ git fetch --all
 git reset --hard origin/master
 git clean -f -d
 
+
 Write-Output ""
 Write-Output "Installeren van extensies..."
 Write-Output "================================================"
 
 foreach ($line in [System.IO.File]::ReadLines("./.vscode/extensions.txt")) {
 	Write-Output "Installeren van extensie '$line'..."
-	code -r --install-extension $line --force
+	code -r --install-extension $line --force --extensions-dir "./.vscode/ext"
 }
 
 Write-Output ""
 Write-Output "Alles is voltooid."
+
+code . --user-data-dir "./.vscode/" --extensions-dir "./.vscode/ext" "./opdracht/index.html" "./opdracht/style.css"
